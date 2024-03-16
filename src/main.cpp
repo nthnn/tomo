affb5c45530af8a64d46fb4715b030f11f4f96b5
nthnn <nathanneisip@gmail.com>
2024-03-17 04:18:12 +0800

    Initial main source file.

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <scenes/tomo_blink.h>
+#include <scenes/tomo_left.h>
+#include <scenes/tomo_right.h>
+
+#include <tomo_display.h>
+#include <tomo_scene.h>
+
+void setup() {
+    TomoScene::initializeTouchPin();
+    if(!TomoDisplay::initialize())
+        while(true);
+
+    TomoDisplay::renderSplashScreen();
+    TomoScene::renderScene<TomoBlinkScene>(3);
+    TomoScene::renderScene<TomoLeftScene>(1);
+    TomoScene::renderScene<TomoRightScene>(1);
+    TomoScene::renderScene<TomoBlinkScene>(2);
+}
+
+void loop() {
+    vTaskDelay(10);
+}
